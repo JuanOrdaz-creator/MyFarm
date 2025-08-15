81bafb3d1bcec2f94c46a84acfc2dea1d7956022
JuanOrdaz-creator <juan.ordaz@ucic.edu.mx>
2025-08-15 17:29:52 -0600

    “v1”

---
+++ Assets/Scripts/InventarioUI.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InventarioUI : MonoBehaviour
+{
+    private Label labelHuevos;
+    private int huevosPrevios = -1;
+
+    private void OnEnable()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        labelHuevos = root.Q<Label>("labelHuevos");
+    }
+
+    private void Update()
+    {
+        if (GameManager.instancia.huevo != huevosPrevios)
+        {
+            // aquí programa la actualización de la interfaz, paso siguiente
+            huevosPrevios = GameManager.instancia.huevo;
+            labelHuevos.text = huevosPrevios.ToString();
+        }
+    }
+}
